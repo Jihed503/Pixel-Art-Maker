feb93611094eea03eb1db863a33b727bbcb19aeb
Jihed503 <54320224+Jihed503@users.noreply.github.com>
2022-01-07 23:40:22 +0100

    Update and rename javascript.js to designs.js

---
+++ designs.js
@@ -0,0 +1,33 @@
+const submit = document.getElementsByTagName('input')[2];
+const table = document.querySelector('table');
+
+submit.addEventListener('click',function makeGrid(e) {
+  const height = document.getElementsByTagName('input')[0].value;
+  const width = document.getElementsByTagName('input')[1].value;
+
+  e.preventDefault();
+  tr='';
+  for (let i=1;i<=height;i++) {  
+    td='';
+    for (let j=1; j<=width; j++) {
+      td += '<td></td>';
+      }
+    tr += '<tr>' + td + '</tr>';
+  }
+  console.log(tr);
+  table.innerHTML = tr;
+})
+
+
+
+table.addEventListener('click',function(e) {
+  const color = document.getElementById('colorPicker').value;
+  e.preventDefault();
+  e.target.style.backgroundColor = color;
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
